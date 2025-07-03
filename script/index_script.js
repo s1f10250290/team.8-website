355ef1d8f105fc1eaf4d15d7565b0bd31940e8eb
s1f10250290 <s1f102502902@iniad.org>
2025-07-03 14:29:57 +0900

    画像遷移のアニメーション

--- script/index_script.js
+++ script/index_script.js
@@ -1,13 +1,16 @@
-const slides = document.querySelectorAll('.slide');
+// .slide-img クラスを持つすべての要素を取得
+const slides = document.querySelectorAll('.slide-img');
 let currentIndex = 0;
 
 function showNextSlide() {
-  // 現在の画像を非表示
-  slides[currentIndex].style.opacity = 0;
+    // 現在表示されている画像を非表示にする
+    slides[currentIndex].classList.remove('active');
 
-  // 次の画像を表示
-  currentIndex = (currentIndex + 1) % slides.length;
-  slides[currentIndex].style.opacity = 1;
+    // 次のインデックスを計算
+    currentIndex = (currentIndex + 1) % slides.length;
+
+    // 次の画像を表示する
+    slides[currentIndex].classList.add('active');
 }
 
 // 3秒ごとに画像を切り替え
